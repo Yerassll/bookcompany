8c4c9e6311f617ab2e9b1f53e6cfac04a79d7228
Yerassyl <erlanulyerasyl433@mail.ru>
2025-04-05 15:39:35 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Book структура
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+// Временное хранилище (имитируем базу данных)
+var books []Book
+
+// Получить все книги (READ)
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Получить одну книгу по ID (READ)
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, book := range books {
+		if book.ID == params["id"] {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+// Создать книгу (CREATE)
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000)) // генерация случайного ID
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+// Обновить книгу (UPDATE)
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+// Удалить книгу (DELETE)
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			json.NewEncoder(w).Encode(books)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+// Главная функция
+func main() {
+	r := mux.NewRouter()
+
+	// Демо-данные
+	books = append(books, Book{ID: "1", Title: "1984", Author: "George Orwell"})
+	books = append(books, Book{ID: "2", Title: "The Hobbit", Author: "J.R.R. Tolkien"})
+
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	fmt.Println("Server is running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
